2d1cee800a869242321dc338508ff46f9cbbcd0f
Nikola <66076379+DzoniBatana@users.noreply.github.com>
2024-05-31 22:28:33 +0200

    update

--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 import { applyMiddleware, combineReducers, createStore } from "redux";
 import { thunk } from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
+import { devToolsEnhancer } from "redux-devtools-extension/logOnlyInProduction";
+
 import accountReducer from "./features/account/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
 
@@ -11,7 +12,7 @@ const rootReducer = combineReducers({
 
 const store = createStore(
   rootReducer,
-  composeWithDevTools(applyMiddleware(thunk))
+  devToolsEnhancer(applyMiddleware(thunk))
 );
 
 export default store;
